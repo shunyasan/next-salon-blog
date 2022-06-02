e9b255e91204d53b364f51a61bbab905f9097c31
高橋駿也 <>
2022-06-02 21:38:03 +0900

    初回デプロイ

--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -1,10 +1,10 @@
 // prod
 export const baseURL =
   "https://k9rjklooj6.execute-api.ap-northeast-1.amazonaws.com/dev/";
-// export const thisURL = "https://anata-epilation/";
+export const thisURL = "https://next-salon-blog/";
 
 // local
 // export const baseURL = "http://localhost:3002/";
-export const thisURL = "http://localhost:3000/";
+// export const thisURL = "http://localhost:3000/";
 
 export const apiKey: string = "XL4wvdOlBnaBXfqw7555P4vFqxm1wwRi8bALfHLp";
