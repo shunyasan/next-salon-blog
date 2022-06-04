3e7e98d108c9f096dccb0206a0f96a862e618e4a
高橋駿也 <>
2022-06-04 14:01:42 +0900

    adsence 修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,19 @@ import { Layout } from "../components/templete/lauouts/Layout";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../../styles/theme/theme";
 import Fonts from "../../styles/theme/fonts";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2833905872269108"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
